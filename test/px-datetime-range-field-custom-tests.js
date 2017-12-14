8bb37c2d3adaa86572125fd6038f2ee2df2e6dd6
Randy Askin <randall.askin@ge.com>
2017-12-14 12:17:52 -0800

    tweaks to tests, add dateFormat back in

--- test/px-datetime-range-field-custom-tests.js
+++ test/px-datetime-range-field-custom-tests.js
@@ -1,5 +1,4 @@
 suite('Navigation', function() {
-
   let range, fireKeyboardEvent;
 
   setup(function(done) {
@@ -13,73 +12,60 @@ suite('Navigation', function() {
     });
   });
 
-  test('navigation from from to to', function(done) {
+  test('navigation from "from" to "to"', function(done) {
     var fields = Polymer.dom(range.root).querySelectorAll('px-datetime-field'),
         fromEntries = Polymer.dom(fields[0].root).querySelectorAll('px-datetime-entry'),
-        fromTimeCells = Polymer.dom(fromEntries[1].root).querySelectorAll('px-datetime-entry-cell');
-    var spy = sinon.spy(range, '_focusFirstToEntry');
-
+        fromTimeCells = Polymer.dom(fromEntries[1].root).querySelectorAll('px-datetime-entry-cell'),
+        spy = sinon.spy(range, '_focusFirstToEntry');
     fireKeyboardEvent(fromTimeCells[fromTimeCells.length - 1], 'ArrowRight');
-
     setTimeout(function() {
       assert.isTrue(spy.called);
-
-      //unwrap spy
       range._focusFirstToEntry.restore();
       done();
     },100);
   });
 
-  test('navigation from from to to doesnt trigger next-field from the outside', function(done) {
+  test('navigation from "from" to "to" doesnt trigger next-field from the outside', function(done) {
     var fields = Polymer.dom(range.root).querySelectorAll('px-datetime-field'),
         fromEntries = Polymer.dom(fields[0].root).querySelectorAll('px-datetime-entry'),
-        fromTimeCells = Polymer.dom(fromEntries[1].root).querySelectorAll('px-datetime-entry-cell');
-
-    var listener = function() {
-      range.removeEventListener('px-next-field', listener);
-      assert.isTrue(false);
-      done();
-    };
+        fromTimeCells = Polymer.dom(fromEntries[1].root).querySelectorAll('px-datetime-entry-cell'),
+        listener = function() {
+          range.removeEventListener('px-next-field', listener);
+          assert.isTrue(false); // makes sure this *doesn't* get run
+          done();
+        };
     range.addEventListener('px-next-field', listener);
-
     fireKeyboardEvent(fromTimeCells[fromTimeCells.length - 1], 'ArrowRight');
-
     setTimeout(function() {
       range.removeEventListener('px-next-field', listener);
       done();
     }, 200);
   });
 
-  test('navigation from to to from', function(done) {
+  test('navigation from "to" to "from"', function(done) {
     var fields = Polymer.dom(range.root).querySelectorAll('px-datetime-field'),
         toEntries = Polymer.dom(fields[1].root).querySelectorAll('px-datetime-entry'),
-        todateCells = Polymer.dom(toEntries[0].root).querySelectorAll('px-datetime-entry-cell');
-    var spy = sinon.spy(range, '_focusLastFromEntry');
-
+        todateCells = Polymer.dom(toEntries[0].root).querySelectorAll('px-datetime-entry-cell'),
+        spy = sinon.spy(range, '_focusLastFromEntry');
     fireKeyboardEvent(todateCells[0], 'ArrowLeft');
-
     setTimeout(function() {
       assert.isTrue(spy.called);
-
-      //unwrap spy
       range._focusLastFromEntry.restore();
       done();
     },100);
   });
 
-  test('navigation from to to from doesnt trigger previous-field from the outside', function(done) {
+  test('navigation from "to" to "from" doesnt trigger previous-field from the outside', function(done) {
     var fields = Polymer.dom(range.root).querySelectorAll('px-datetime-field'),
         toEntries = Polymer.dom(fields[1].root).querySelectorAll('px-datetime-entry'),
-        todateCells = Polymer.dom(toEntries[0].root).querySelectorAll('px-datetime-entry-cell');
-
-    var listener = function() {
-      range.removeEventListener('px-previous-field', listener);
-      done();
-    };
+        todateCells = Polymer.dom(toEntries[0].root).querySelectorAll('px-datetime-entry-cell'),
+        listener = function() {
+          range.removeEventListener('px-previous-field', listener);
+          assert.isTrue(false); // makes sure this *doesn't* get run
+          done();
+        };
     range.addEventListener('px-previous-field', listener);
-
     fireKeyboardEvent(todateCells[0], 'ArrowLeft');
-
     setTimeout(function() {
       range.removeEventListener('px-previous-field', listener);
       done();
@@ -94,7 +80,6 @@ suite('submit without buttons', function() {
     range = fixture('range');
     range.fromMoment = Px.moment("2017-10-01T00:03:44.339Z");
     range.toMoment = Px.moment("2017-10-14T00:03:44.339Z");
-
     flush(()=>{
       //make sure we focus 'to' field as next tests work on 'to'
       fields = Polymer.dom(range.root).querySelectorAll('px-datetime-field');
@@ -109,25 +94,23 @@ suite('submit without buttons', function() {
   test('moment is not changed when changing invalid value', function(done) {
     var fields = Polymer.dom(range.root).querySelectorAll('px-datetime-field'),
         toEntries = Polymer.dom(fields[1].root).querySelectorAll('px-datetime-entry'),
+        wrapper = Polymer.dom(fields[1].root).querySelector('#fieldWrapper'),
         todateCells = Polymer.dom(toEntries[0].root).querySelectorAll('px-datetime-entry-cell'),
         dateInput = Polymer.dom(todateCells[1].root).querySelector('input'),
         e = document.createEvent('Event'),
-        i=0;
-
-    var listener = function(evt) {
-      i++;
-    };
-
+        i=0,
+        listener = function(evt) {
+          i++;
+        };
     range.addEventListener('to-moment-changed', listener);
-
     dateInput.value = '99';
     e.initEvent("blur", true, true);
     dateInput.dispatchEvent(e);
-
     setTimeout(function() {
       assert.equal(i, 0);
       assert.isFalse(range.isValid);
-      assert.isTrue(range._isRangeValid);
+      assert.isFalse(range._toValid);
+      assert.isTrue(wrapper.classList.contains('validation-error'));
       range.removeEventListener('to-moment-changed', listener);
       done();
     }, 100);
@@ -136,25 +119,23 @@ suite('submit without buttons', function() {
   test('moment is changed when changing valid value', function(done) {
     var fields = Polymer.dom(range.root).querySelectorAll('px-datetime-field'),
         toEntries = Polymer.dom(fields[1].root).querySelectorAll('px-datetime-entry'),
+        wrapper = Polymer.dom(fields[1].root).querySelector('#fieldWrapper'),
         todateCells = Polymer.dom(toEntries[0].root).querySelectorAll('px-datetime-entry-cell'),
         dateInput = Polymer.dom(todateCells[1].root).querySelector('input'),
         e = document.createEvent('Event'),
         i = 0;
-
     var listener = function(evt) {
       i++;
     };
-
     range.addEventListener('to-moment-changed', listener);
-
     dateInput.value = '12';
     e.initEvent("blur", true, true);
     dateInput.dispatchEvent(e);
-
     setTimeout(function() {
       assert.equal(i, 1);
       assert.isTrue(range.isValid);
-      assert.isTrue(range._isRangeValid);
+      assert.isTrue(range._toValid);
+      assert.isFalse(wrapper.classList.contains('validation-error'));
       range.removeEventListener('to-moment-changed', listener);
       done();
     }, 100);
@@ -163,25 +144,23 @@ suite('submit without buttons', function() {
   test('moment is not changed when range is backwards', function(done) {
     var fields = Polymer.dom(range.root).querySelectorAll('px-datetime-field'),
         toEntries = Polymer.dom(fields[1].root).querySelectorAll('px-datetime-entry'),
+        wrapper = Polymer.dom(fields[1].root).querySelector('#fieldWrapper'),
         todateCells = Polymer.dom(toEntries[0].root).querySelectorAll('px-datetime-entry-cell'),
         dateInput = Polymer.dom(todateCells[1].root).querySelector('input'),
         e = document.createEvent('Event'),
-        i = 0;
-
-    var listener = function(evt) {
-      i++;
-    };
-
+        i = 0,
+        listener = function(evt) {
+          i++;
+        };
     range.addEventListener('to-moment-changed', listener);
-
     dateInput.value = '01';
     e.initEvent("blur", true, true);
     dateInput.dispatchEvent(e);
-
     setTimeout(function() {
       assert.equal(i, 0);
       assert.isFalse(range.isValid);
       assert.isFalse(range._isRangeValid);
+      assert.isTrue(wrapper.classList.contains('validation-error'));
       range.removeEventListener('to-moment-changed', listener);
       done();
     }, 100);
@@ -208,24 +187,20 @@ suite('submit with buttons', function() {
     });
   });
 
-  test('moment is not changed when changing valid value + buttons', function(done) {
+  test('moment is not changed on blur', function(done) {
     var fields = Polymer.dom(range.root).querySelectorAll('px-datetime-field'),
         toEntries = Polymer.dom(fields[1].root).querySelectorAll('px-datetime-entry'),
         todateCells = Polymer.dom(toEntries[0].root).querySelectorAll('px-datetime-entry-cell'),
         dateInput = Polymer.dom(todateCells[1].root).querySelector('input'),
         e = document.createEvent('Event'),
-        i = 0;
-
-    var listener = function(evt) {
-      i++;
-    };
-
+        i = 0,
+        listener = function(evt) {
+          i++;
+        };
     range.addEventListener('to-moment-changed', listener);
-
     dateInput.value = '12';
     e.initEvent("blur", true, true);
     dateInput.dispatchEvent(e);
-
     setTimeout(function() {
       assert.equal(i, 0);
       range.removeEventListener('to-moment-changed', listener);
@@ -234,19 +209,13 @@ suite('submit with buttons', function() {
   });
 
   test('moment is changed when pressing enter', function() {
-    var i = 0;
-
-    var listener = function(evt) {
-      i++;
-    };
-
+    var i = 0,
+        listener = function(evt) {
+          i++;
+        };
     range.addEventListener('from-moment-changed', listener);
-
-    //do a change
     range._tempFromMomentObj = range.fromMoment.clone().subtract(1, 'month');
-
     fireKeyboardEvent(range, 'Enter');
-
     assert.equal(i, 1);
     range.removeEventListener('from-moment-changed', listener);
   });
@@ -254,54 +223,38 @@ suite('submit with buttons', function() {
   test('moment is changed when clicking apply', function() {
     var datetimeButtons = Polymer.dom(range.root).querySelector('px-datetime-buttons'),
         buttons = Polymer.dom(datetimeButtons.root).querySelectorAll('button'),
-        i = 0;
-
-    var listener = function(evt) {
-      i++;
-    };
-
+        i = 0,
+        listener = function(evt) {
+          i++;
+        };
     range.addEventListener('from-moment-changed', listener);
-
-    //do a change
     range._tempFromMomentObj = range.fromMoment.clone().subtract(1, 'month');
-
     buttons[1].click();
-
     assert.equal(i, 1);
     range.removeEventListener('from-moment-changed', listener);
   });
 
-  test('moment is rolledback when clicking cancel', function(done) {
+  test('moment is rolled back when clicking cancel', function(done) {
     var datetimeButtons = Polymer.dom(range.root).querySelector('px-datetime-buttons'),
         buttons = Polymer.dom(datetimeButtons.root).querySelectorAll('button'),
         prevFromMoment = range.fromMoment.clone(),
         i = 0;
-
-    //do a change
     range._tempFromMomentObj = range.fromMoment.clone().subtract(1, 'month');
-
     buttons[0].click();
-
     flush(function() {
       assert.equal(range.fromMoment.toISOString(), prevFromMoment.toISOString());
       done();
     });
-
   });
 
-  test('moment is rolledback when pressing esc', function(done) {
+  test('moment is rolled back when pressing esc', function(done) {
     var datetimeButtons = Polymer.dom(range.root).querySelector('px-datetime-buttons'),
-        buttons = Polymer.dom(datetimeButtons.root).querySelectorAll('button'),
         prevFromMoment = range.fromMoment.clone();
-
     range._tempFromMomentObj = range.fromMoment.clone().subtract(1, 'month');
-
     fireKeyboardEvent(range, 'Esc');
-
     flush(function() {
       assert.equal(range.fromMoment.toISOString(), prevFromMoment.toISOString());
       done();
     });
-
   });
 });
